552c01b56d529b76ceb249a50cead29fe5eef537
Sebashdz708 <j.hernandezv@uniandes.edu.co>
2022-02-10 22:25:19 -0500

    cambios del Estudiante 1

--- App/view.py
+++ App/view.py
@@ -43,6 +43,7 @@ def printMenu():
     print("1- Cargar Libros")
     print("2- Cargar Tags")
     # TODO: Modificación de Est-1 en el Lab 2
+    print("3- Cargar Book-Tags!")
     print("0- Salir")
 
 
@@ -65,7 +66,7 @@ def loadBookTags():
     Cargar los Tags de libros
     """
     # TODO: Modificación de Est-1 en el Lab 2
-    pass
+    return controller.loadBooks('GoodReads/book_tags-small.csv')
 
 
 """
